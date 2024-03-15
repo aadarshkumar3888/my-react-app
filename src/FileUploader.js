5152db23aafc59f09acebfd2d35e88e1b05eff1e
dhruvnagpalblc <dhruv.nagpal@Brightlifecare.com>
2024-03-15 21:10:25 +0530

    commit

--- src/FileUploader.js
+++ src/FileUploader.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
-import axios from 'axios'; // Import Axios
-import './FileUploader.css'; // Import the CSS file if styles are kept in a separate file
+import axios from 'axios';
 
-function FileUploader() {
+function ExcelUploader() {
     const [file1, setFile1] = useState(null);
     const [file2, setFile2] = useState(null);
-    const [result, setResult] = useState(null);
+    const [resultData, setResultData] = useState({});
+    const [editedData, setEditedData] = useState({});
+    const [selectedValue1, setSelectedValue1] = useState('');
+    const [selectedValue2, setSelectedValue2] = useState('');
+    const [showSubmitButton, setShowSubmitButton] = useState(false); // State to control visibility of the submit button
+    const [showDropdowns, setShowDropdowns] = useState(false); // State to control visibility of the dropdowns
 
     const handleFile1Change = (event) => {
         setFile1(event.target.files[0]);
@@ -15,58 +19,52 @@ function FileUploader() {
         setFile2(event.target.files[0]);
     };
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
 
         const formData = new FormData();
         formData.append('file1', file1);
         formData.append('file2', file2);
 
-        try {
-            const response = await axios.post('/upload', formData); // Use Axios for POST request
+        axios.post('http://localhost:8080/recon/test3', formData)
+            .then(response => {
+                setResultData(response.data);
 
-            if (!response.status === 200) {
-                throw new Error('Upload failed');
-            }
-            console.log('Response:', response.data);
-            const data = response.data;
-            setResult(data);
-        } catch (error) {
-            console.error('Error:', error);
-            setResult('An error occurred while uploading files.');
-        }
+                // Show the submit button and dropdowns after data is received
+                setShowSubmitButton(true);
+                setShowDropdowns(true);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                setResultData({ error: 'An error occurred while uploading files.' });
+            });
     };
 
     const handleEditSubmit = () => {
-        const editedData = {}; // Object to store edited data
+        // Merge resultData and editedData
+        const mergedData = { ...resultData, ...editedData };
 
-        // Get edited data from input fields
-        const inputs = document.querySelectorAll('#result input[type="text"]');
-        for (let i = 0; i < inputs.length; i += 2) {
-            editedData[inputs[i].value] = inputs[i + 1].value;
-        }
+        // Append selected values
+        mergedData['selectedValue1'] = selectedValue1;
+        mergedData['selectedValue2'] = selectedValue2;
 
-        // Get selected values from dropdowns
-        const select1 = document.querySelector('#result select:nth-of-type(1)');
-        const select2 = document.querySelector('#result select:nth-of-type(2)');
-        editedData['selectedValue1'] = select1.value;
-        editedData['selectedValue2'] = select2.value;
-
-        // Send edited data to the backend using Axios
-        axios.post('/submitEditedData', editedData)
-        .then(response => {
-            // Log response from the backend
-            console.log('Edited data submitted successfully:', response.data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+        // Send merged data to the backend
+        axios.post('http://localhost:8080/recon/test4', mergedData)
+            .then(response => {
+                // Handle response from the backend if needed
+                console.log('Edited data submitted successfully:', response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     };
 
+
+
     return (
-        <div className="file-uploader">
+        <div>
             <h2>Upload Excel Files</h2>
-            <form onSubmit={handleSubmit} encType="multipart/form-data" className="form">
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
                 <div>
                     <label htmlFor="file1">Select Excel File 1:</label>
                     <input type="file" id="file1" name="file1" accept=".xlsx, .xls" onChange={handleFile1Change} />
@@ -77,13 +75,66 @@ function FileUploader() {
                 </div>
                 <button type="submit">Upload</button>
             </form>
-            <div id="result" className="result"></div>
-
-            <div id="submitEditedData" className="submit-edited-data">
-                <button id="submitEditedButton" type="button" onClick={handleEditSubmit}>Submit Edited Data</button>
+            <div id="result">
+                {/* Display result data */}
+                {Object.keys(resultData).map((key, index) => (
+                    <div key={index}>
+                        <label>Key:</label>
+                        <input
+                            type="text"
+                            value={editedData[key]?.key || key} // If key is edited, show edited key, otherwise show original key
+                            onChange={(event) => {
+                                const newKey = event.target.value;
+                                setEditedData(prevState => ({
+                                    ...prevState,
+                                    [key]: { key: newKey, value: resultData[key] }
+                                }));
+                            }}
+                        />
+                        <label>Value:</label>
+                        <input
+                            type="text"
+                            value={editedData[key]?.value || resultData[key]} // If value is edited, show edited value, otherwise show original value
+                            onChange={(event) => {
+                                const newValue = event.target.value;
+                                setEditedData(prevState => ({
+                                    ...prevState,
+                                    [key]: { key: editedData[key]?.key || key, value: newValue } // Use edited key if available
+                                }));
+                            }}
+                        />
+                        <br />
+                    </div>
+                ))}
+                {/* Dropdowns for selected values */}
+                {showDropdowns && (
+                    <>
+                        <select value={selectedValue1} onChange={(event) => setSelectedValue1(event.target.value)}>
+                            {Object.keys(resultData).map((key, index) => (
+                                <option key={index} value={resultData[key]}>
+                                    {key}
+                                </option>
+                            ))}
+                        </select>
+                        <select value={selectedValue2} onChange={(event) => setSelectedValue2(event.target.value)}>
+                            {Object.keys(resultData).map((key, index) => (
+                                <option key={index} value={resultData[key]}>
+                                    {key}
+                                </option>
+                            ))}
+                        </select>
+                    </>
+                )}
             </div>
+            {showSubmitButton && (
+                <div id="submitEditedData">
+                    <button id="submitEditedButton" type="button" onClick={handleEditSubmit}>
+                        Submit Edited Data
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
 
-export default FileUploader;
+export default ExcelUploader;
